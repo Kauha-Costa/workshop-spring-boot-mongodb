34f83d95d7d37f4a6b896442c7c10a473802d841
Kauha Costa <kauhaerickcosta@gmail.com>
2025-03-18 14:36:12 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/kauhacosta/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kauhacosta.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.kauhacosta.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
