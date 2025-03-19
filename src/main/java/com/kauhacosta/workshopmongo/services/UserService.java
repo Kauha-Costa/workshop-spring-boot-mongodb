152d966e6a29d829d1a590b00ad77f9484f305c7
Kauha Costa <kauhaerickcosta@gmail.com>
2025-03-19 13:26:22 -0300

    Insercao de usuario com POST

--- src/main/java/com/kauhacosta/workshopmongo/services/UserService.java
+++ src/main/java/com/kauhacosta/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kauhacosta.workshopmongo.domain.User;
+import com.kauhacosta.workshopmongo.dto.UserDTO;
 import com.kauhacosta.workshopmongo.repository.UserRepository;
 import com.kauhacosta.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,21 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
